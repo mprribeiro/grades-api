5569b487bab6a46279a81c1ba6511a9c10a2bc73
Marcos Ribeiro <mprribeiro1902@gmail.com>
2020-07-06 14:55:26 -0300

    Correçao parametros de comunicaçao

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://peaceful-meadow-85917.herokuapp.com/',
   })
 );
 
